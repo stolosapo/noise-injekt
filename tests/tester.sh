06cfa27fe93377e4d71bd37b2445857939579cf0
Apostolos <aldrigmej@yahoo.gr>
2022-05-27 21:27:27 +0300

    Added Service files

--- tests/tester.sh
+++ tests/tester.sh
@@ -2,6 +2,8 @@
 
 g++ -g -c -o main.o main.cpp
 g++ -g -c -o MainTestSuite.o MainTestSuite.cpp
+g++ -g -c -o Service.o ../src/Service.cpp
+g++ -g -c -o ServiceFactory.o ../src/ServiceFactory.cpp
 
 g++ main.o \
     MainTestSuite.o \
