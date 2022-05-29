06b8e8b124372f33a1317c501a1a6d82bdaf5537
Apostolos <aldrigmej@yahoo.gr>
2022-05-29 20:15:25 +0300

    Added smoke usage in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,55 @@
 #include <iostream>
+#include "GlobalAppContext.h"
 
 using namespace std;
+using namespace NoiseInjekt;
+
+void mainRegisterServiceDelegate(int argc, char* argv[]);
+void mainFinilizeServiceDelegate();
+
+class TestService: public Service
+{
+public:
+    TestService(): Service() { }
+    virtual ~TestService() { }
+    void test()
+    {
+        cout << "This is a Service Test" << endl;
+    } 
+};
+
+class TestServiceFactory: public ServiceFactory
+{
+public:
+	TestServiceFactory(): ServiceFactory() { }
+	Service *create()
+    {
+        return (Service*) new TestService;
+    }
+};
 
 int main(int argc, char* argv[])
 {
     cout << "noiseinjekt Library" << endl << endl;
+
+    initializeAppContext(&mainRegisterServiceDelegate, argc, argv);
+
+    TestService* srv = inject<TestService>("TestService");
+    srv->test();
+
+    deleteAppContext(&mainFinilizeServiceDelegate);
+
     cout << "Bye Bye.." << endl;
+}
+
+void mainRegisterServiceDelegate(int argc, char* argv[])
+{
+    registerGlobalService<TestServiceFactory>(SINGLETON, "TestService", new TestServiceFactory);
+
+    cout << "Initialized" << endl;
+}
+
+void mainFinilizeServiceDelegate()
+{
+    cout << "Finalized" << endl;
 }
